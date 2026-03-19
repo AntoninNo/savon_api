7fbd1e7b34e08bc155cb80668a6d42ad4c9c48d5
AntoninNo <noel.antoninn@gmail.com>
2026-03-19 12:01:12 +0100

    suite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { LoginPage } from './pages/login-page/login-page';
 import { SubscribePage } from './pages/subscribe-page/subscribe-page';
 import { AccountManagerPage } from './pages/account-manager-page/account-manager-page';
 import { RecipeCalculatorPage } from './pages/recipe-calculator-page/recipe-calculator-page';
-import { RecipeManagerPage } from './pages/recipe-manager-page/recipe-manager-page';
+import { RecettesManagerPage } from './pages/recipe-manager-page/recipe-manager-page';
 import { UsersManagerPage } from './pages/users-manager-page/users-manager-page';
-import { IngredientsManagerPage } from './pages/ingredients-manager-page / ingredients-manager-page';
+import { IngredientsManagerPage } from './pages/ingredients-manager-page/ingredients-manager-page';
 import { AboutPage } from './pages/about-page/about-page';
 import { LegalNoticePage } from './pages/legal-notice-page/legal-notice-page';
 import { authGuard } from './guards/auth-guard';
 export const routes: Routes = [
+
     // Accès & redirection vers la page home :
     { path: '', pathMatch: 'full', redirectTo: 'home' }, // Route de la racine :
     { path: 'home', component: HomePage },
